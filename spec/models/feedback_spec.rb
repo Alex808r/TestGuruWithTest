55d950d5356e4757e8ccd46295c4b8fd4968299c
Alex808r <fromxlam@gmail.com>
2021-12-27 12:30:37 +0300

    Fix style test model Feedback

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -15,16 +15,15 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  
   describe 'validations' do
     it { should validate_presence_of :user_name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :header }
     it { should validate_presence_of :content }
   end
-  
+
   describe 'database' do
-    it { should have_db_column(:id).of_type(:integer).with_options(primary: true)}
+    it { should have_db_column(:id).of_type(:integer).with_options(primary: true) }
     it { should have_db_column(:id).with_options(null: false) }
 
     it { should have_db_column(:content).of_type(:string).with_options(null: false) }
